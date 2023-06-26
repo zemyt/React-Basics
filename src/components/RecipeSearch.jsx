d95d0ff5bcef71d4f1959893dcc6b527f41cdd14
Zemyt <tijme_@hotmail.com>
2023-06-26 14:38:10 +0200

    added filter

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -1,26 +1,77 @@
 /* eslint-disable react/prop-types */
-import { Center, Input, Wrap } from "@chakra-ui/react";
+import { Center, Input, Wrap, Radio, RadioGroup } from "@chakra-ui/react";
 import { data } from "../utils/data";
 import React from "react";
 import { RecipeList } from "./RecipeList";
 import { useState } from "react";
+import { EmptyCard } from "./EmptyCard";
 
 export const RecipeSearch = ({ clickFn }) => {
   const recipes = data.hits.map((item) => item.recipe);
   const [searchField, setSearchField] = useState("");
+  const [searchValue, setSeatchValue] = useState("1");
 
   const handleChange = (event) => {
     setSearchField(event.target.value);
   };
 
   const matchedRecipes = recipes.filter((recipe) => {
-    const isLabelMatched = recipe.label
-      .toLowerCase()
-      .includes(searchField.toLowerCase());
-    const areHealthLabelsMatched = recipe.healthLabels.some((label) =>
-      label.toLowerCase().includes(searchField.toLowerCase())
-    );
-    return isLabelMatched || areHealthLabelsMatched;
+    let isLabelMatched = [];
+    let areHealthLabelsMatched = [];
+
+    switch (searchValue) {
+      case "1":
+        // Returns matched results from all the recipes
+        isLabelMatched = recipe.label
+          .toLowerCase()
+          .includes(searchField.toLowerCase());
+        areHealthLabelsMatched = recipe.healthLabels.some((label) =>
+          label.toLowerCase().includes(searchField.toLowerCase())
+        );
+        return isLabelMatched || areHealthLabelsMatched;
+
+      case "2":
+        // Returns matched results from the vegan recipes
+        if (recipe.healthLabels.includes("Vegan")) {
+          isLabelMatched = recipe.label
+            .toLowerCase()
+            .includes(searchField.toLowerCase());
+          areHealthLabelsMatched = recipe.healthLabels.some((label) =>
+            label.toLowerCase().includes(searchField.toLowerCase())
+          );
+          return isLabelMatched || areHealthLabelsMatched;
+        }
+        break;
+
+      case "3":
+        // Returns matched results from the vegetarian recipes
+        if (recipe.healthLabels.includes("Vegetarian")) {
+          isLabelMatched = recipe.label
+            .toLowerCase()
+            .includes(searchField.toLowerCase());
+          areHealthLabelsMatched = recipe.healthLabels.some((label) =>
+            label.toLowerCase().includes(searchField.toLowerCase())
+          );
+          return isLabelMatched || areHealthLabelsMatched;
+        }
+        break;
+
+      case "4":
+        // Returns matched results from the pescatarian recipes
+        if (recipe.healthLabels.includes("Pescatarian")) {
+          isLabelMatched = recipe.label
+            .toLowerCase()
+            .includes(searchField.toLowerCase());
+          areHealthLabelsMatched = recipe.healthLabels.some((label) =>
+            label.toLowerCase().includes(searchField.toLowerCase())
+          );
+          return isLabelMatched || areHealthLabelsMatched;
+        }
+        break;
+
+      default:
+        return isLabelMatched || areHealthLabelsMatched;
+    }
   });
 
   return (
@@ -33,13 +84,36 @@ export const RecipeSearch = ({ clickFn }) => {
           placeholder="Search for recipe..."
           backgroundColor="white"
           mt="10px"
+          mb="10px"
           shadow="lg"
           onChange={handleChange}
         />
       </Center>
+
+      <Center>
+        <RadioGroup onChange={setSeatchValue} value={searchValue}>
+          <Wrap marginLeft="10px" marginRight="10px" justify="center">
+            <Radio value="1">All</Radio>
+            <Radio value="2">Vegan</Radio>
+            <Radio value="3">Vegetarian</Radio>
+            <Radio value="4">Pescatarian</Radio>
+          </Wrap>
+        </RadioGroup>
+      </Center>
+
       <Wrap padding="2rem" justify="center" spacing="2rem">
-        <RecipeList recipes={matchedRecipes} clickFn={clickFn} />
+        {matchedRecipes.length > 0 ? (
+          <RecipeList recipes={matchedRecipes} clickFn={clickFn} />
+        ) : (
+          <EmptyCard />
+        )}
       </Wrap>
     </>
   );
 };
+
+{
+  /* <Wrap padding="2rem" justify="center" spacing="2rem">
+  <RecipeList recipes={matchedRecipes} clickFn={clickFn} />
+</Wrap>; */
+}
